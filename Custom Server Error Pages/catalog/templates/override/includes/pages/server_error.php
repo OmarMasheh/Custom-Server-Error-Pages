0af0d4c99132b349af0bf9d4aa592134e2e87e4b
Omar Masheh <36455369+OmarMasheh@users.noreply.github.com>
2021-08-07 13:44:59 +0300

    Update server_error.php

--- Custom Server Error Pages/catalog/templates/override/includes/pages/server_error.php
+++ Custom Server Error Pages/catalog/templates/override/includes/pages/server_error.php
@@ -1,5 +1,13 @@
 <?php
 
+/*
+  $Id$
+  CE Phoenix, E-Commerce made Easy
+  https://phoenixcart.org
+  Copyright (c) 2021 Phoenix Cart
+  Released under the GNU General Public License
+  author Jack_mcs
+*/
 
   require $oscTemplate->map_to_template('template_top.php', 'component');
 
